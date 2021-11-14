cfcb9595a7c9dbd5e4b4a337c2b1600c9e8113cf
MikolajKolek <68162257+MikolajKolek@users.noreply.github.com>
2021-11-14 17:11:18 +0100

    Added dotnet version check to the dotnet entrypoint

--- dotnet/entrypoint.sh
+++ dotnet/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/container
 
+# Output Current Dotnet Version
+dotnet --version
+
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
